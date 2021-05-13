091e648da154e59af2348bc530c1ab2894d0903e
Aline Gimenez de Castro <alinegimenezdecastro@hotmail.com>
2021-05-13 02:11:39 +0000

    Login e cadstro usuario realizado

--- database.js
+++ database.js
@@ -13,12 +13,40 @@ const pool = new Pool({
 //const script = 'CREATE TABLE IF NOT EXISTS motorista (usuario_codigo char(36) not null,	cnh char(11) not null, placa_van char(7) not null, modelo_van varchar(20) not null,	cor_van varchar(20) not null, marca_van varchar(20) not null, constraint moto_pk_usuario_cod primary key(usuario_codigo), constraint moto_fk_usuario_cod foreign key(usuario_codigo) references usuario, constraint moto_cnh_uk unique(cnh), constraint moto_placa_van_uk unique(placa_van))';
 //const script = 'CREATE TABLE IF NOT EXISTS aluno (usuario_codigo char(36) not null,	nome_aluno varchar(50) not null, endereco varchar(50) not null,	trajeto int not null, escola varchar(20) not null, endereco_escola varchar(50) not null, codigo_motorista char(36),	constraint aluno_pk_usuario_cod primary key(usuario_codigo), constraint aluno_fk_usuario_cod foreign key(usuario_codigo) references usuario, constraint aluno_fk_motorista_cod foreign key(codigo_motorista) references motorista, constraint aluno_valida_trajeto check(trajeto in (1,2,3)))';
 //const script = 'CREATE TABLE IF NOT EXISTS ausente (uuid char(36) not null, aluno_codigo char(36) not null, turno_ida int not null, turno_volta int not null, constraint ausente_pk primary key(uuid), constraint ausente_fk_aluno_cod foreign key(aluno_codigo) references aluno, constraint ausente_valida_turno_ida check(turno_ida in (0,1)), constraint ausente_valida_turno_volta check(turno_volta in (0,1)))';
-const script = 'CREATE TABLE IF NOT EXISTS turno (uuid char(36) not null, aluno_codigo char(36) not null, motorista_codigo char(36) not null, turno varchar(20) not null, status_turno int not null, constraint turno_pk primary key(uuid), constraint turno_fk_aluno_cod foreign key(aluno_codigo) references aluno, constraint turno_fk_motorista_cod foreign key(motorista_codigo) references motorista, constraint turno_valida_status check(status_turno in (0,1,2,3)))';
+//const script = 'CREATE TABLE IF NOT EXISTS turno (uuid char(36) not null, aluno_codigo char(36) not null, motorista_codigo char(36) not null, turno varchar(20) not null, status_turno int not null, constraint turno_pk primary key(uuid), constraint turno_fk_aluno_cod foreign key(aluno_codigo) references aluno, constraint turno_fk_motorista_cod foreign key(motorista_codigo) references motorista, constraint turno_valida_status check(status_turno in (0,1,2,3)))';
 
 
-pool.query(script, function (error, result) {
+/*pool.query(script, function (error, result) {
     if (error)
         throw error;
     else
         console.log("Tabela criada com sucesso.");
-})
+})*/
+
+module.exports = {
+
+    async login(login, senha) {
+        const sql = `SELECT usuario_tipo FROM usuario WHERE usuario_login = $1 and usuario_password = $2`;
+        const result = await pool.query(sql, [login, senha]);
+        //print(result);
+        return result.rows;
+    },
+
+    async createUsuario(uuid, nome_usuario, cpf, rg, telefone, usuario_login, usuario_password, usuario_tipo) {
+        try {
+            const sql = `INSERT INTO usuario (uuid, nome_usuario, cpf, rg, telefone, usuario_login, usuario_password, usuario_tipo) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`;
+            const result = await pool.query(sql, [uuid, nome_usuario, cpf, rg, telefone, usuario_login, usuario_password, usuario_tipo]);
+            return result.rows;
+
+        }catch(error) {
+            console.log(error);
+            return -1;
+        }
+    },
+
+    async read() {
+        const sql = `SELECT * FROM usuario`;
+        const result = await pool.query(sql);
+        return result.rows;
+    },
+}
--- server.js
+++ server.js
@@ -1,9 +1,51 @@
-const express = require('express');
-
+const  express  =  require ('express') ;  // import express; usando express;
 const server = express();
+const cors = require ('cors');
+const { uuid } = require('uuidv4');
 
-server.get('/', function(request, response) {
-    response.send('Hello World!');
+const database = require('./database');
+
+server.use(cors())
+server.use(express.json())
+
+server.get('/login', async function(request, response) {
+    const login = request.body.login;
+    const password = request.body.password;
+    const resposta = await database.login(login, password);
+    if(resposta != ""){
+        response.json(resposta);
+        response.status(200).send();
+    }
+    else{
+        response.json("SENHA ERRADA");
+        response.status(400).send();
+    }
+    
 })
+
+server.get('/', async function(request, response) {
+    const resultado = await database.read();
+    response.json(resultado);
+})
+
+server.post('/cadastrousuario', async function(request, response) {
+    const uuid = '4';
+    const nome_usuario = request.body.nome;
+    const cpf = request.body.cpf;
+    const rg = request.body.rg;
+    const telefone = request.body.telefone;
+    const usuario_login = request.body.login;
+    const usuario_password = request.body.password;
+    const tipo_usuario = request.body.tipo;
+
+    const result = await database.createUsuario(uuid, nome_usuario, cpf, rg, telefone, usuario_login, usuario_password, tipo_usuario);
+    response.status(200).send();
+})
+
+
+
+/*server.get('/', function(request, response) {
+    response.send('Hello World!');
+})*/
 
 server.listen(process.env.PORT || 3000);
